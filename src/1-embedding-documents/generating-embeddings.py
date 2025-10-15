bde94b67781a686fc25fb567b7ce3b1d73d567e2
AXLMRIN <axelmorinmail@gmail.com>
2025-10-15 13:27:53 +0000

    adapt new data structure

--- src/1-embedding-documents/generating-embeddings.py
+++ src/1-embedding-documents/generating-embeddings.py
@@ -4,20 +4,20 @@ from transformers import AutoTokenizer
 
 from ExportEmbeddingsClass import ExportEmbeddings
 
+language = "fr"
 # ====================================================================================
-df = pd.read_csv("./theses/theses-soutenues-clean-with-index-to-embed.csv")
-text_col = "resumes.fr"
-cluster_col = "oai_first"
-# model_name = "Qwen/Qwen3-Embedding-0.6B" # multilingual # DONE : EN - FR
+df = pd.read_csv("./data/theses-to-embed.csv").sample(frac = 1)
+text_col = f"resumes.{language}"
+# model_name = "Qwen/Qwen3-Embedding-0.6B" # multilingual 
 # model_name = "sentence-transformers/all-MiniLM-L6-v2"  # multilingual
-# model_name = "Alibaba-NLP/gte-multilingual-base" # multilingual # DONE: EN - FR
-# model_name = "codefuse-ai/F2LLM-0.6B" # english # DONE: EN
+model_name = "Alibaba-NLP/gte-multilingual-base" # multilingual 
+# model_name = "codefuse-ai/F2LLM-0.6B" # english 
 # model_name = "Sahajtomar/french_semantic" # french
 # model_name = "camembert/camembert-base" # fr
-model_name = "almanach/camembertav2-base"  # fr
+# model_name = "almanach/camembertav2-base" # fr
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer_parameters = {"padding": "max_length", "truncation": True, "max_length": 1000}
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code = True)
+tokenizer_parameters = {"padding": "max_length", "truncation": True, "max_length": 1500}
 
 
 def tokenization(row):
@@ -32,12 +32,14 @@ df = df.apply(tokenization, axis=1)
 
 additional_tags = [
     "CI",
-    "oai_first_name",
-    "resumes.fr",
-    "sujets_rameau_fr",
-    "resumes.en",
-    "sujets_rameau_en",
     "year",
+    "oai_set_specs",
+    
+    "resumes.en",
+    "topics.en",
+
+    "resumes.fr",
+    "topics.fr",
 ]
 
 for col in additional_tags:
@@ -53,4 +55,4 @@ ds = ds.select(range(10))
 
 embeddings_exporter = ExportEmbeddings(ds, model_name)
 print("device : ", embeddings_exporter.device)
-embeddings_exporter.routine(2, additional_tags, "./theses/embeddings-fr-camembertav2")
+embeddings_exporter.routine(2, additional_tags, f"./embeddings/{model_name.split('/')[-1]}-{language}")
