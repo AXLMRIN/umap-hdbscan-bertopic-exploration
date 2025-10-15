ad295f7cf42705b9815f28a08e0d34d599f91259
A.Morin <axelmorinmail@gmail.com>
2025-10-15 18:04:29 +0200

    switch to sentenceTransformer

--- src/1-embedding-documents/generating-embeddings.py
+++ src/1-embedding-documents/generating-embeddings.py
@@ -1,56 +1,73 @@
 from datasets import Dataset
 import pandas as pd
-from transformers import AutoTokenizer
+from transformers import AutoConfig
+from sentence_transformers import SentenceTransformer
+from torch.cuda import is_available as cuda_available
 
-from ExportEmbeddingsClass import ExportEmbeddings
+from ExportEmbeddingsClass import clean
 
-# ====================================================================================
-df = pd.read_csv("./theses/theses-soutenues-clean-with-index-to-embed.csv")
-text_col = "resumes.fr"
-cluster_col = "oai_first"
-# model_name = "Qwen/Qwen3-Embedding-0.6B" # multilingual # DONE : EN - FR
-# model_name = "sentence-transformers/all-MiniLM-L6-v2"  # multilingual
-# model_name = "Alibaba-NLP/gte-multilingual-base" # multilingual # DONE: EN - FR
-# model_name = "codefuse-ai/F2LLM-0.6B" # english # DONE: EN
+language = "fr"
+# ==============================================================================
+df = pd.read_csv("./data/theses-to-embed.csv").sample(frac = 1)
+text_col = f"resumes.{language}"
+# model_name = "Qwen/Qwen3-Embedding-0.6B" # multilingual 
+model_name = "sentence-transformers/all-MiniLM-L6-v2"  # multilingual
+# model_name = "Alibaba-NLP/gte-multilingual-base" # multilingual 
+# model_name = "codefuse-ai/F2LLM-0.6B" # english 
 # model_name = "Sahajtomar/french_semantic" # french
 # model_name = "camembert/camembert-base" # fr
-model_name = "almanach/camembertav2-base"  # fr
+# model_name = "almanach/camembertav2-base" # fr
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer_parameters = {"padding": "max_length", "truncation": True, "max_length": 1000}
-
-
-def tokenization(row):
-    tokenizer_output = tokenizer(row[text_col], **tokenizer_parameters)
-    row["input_ids"] = tokenizer_output["input_ids"]
-    row["attention_mask"] = tokenizer_output["attention_mask"]
-    row["n_tokens"] = sum(tokenizer_output["attention_mask"])
-    return row
-
-
-df = df.apply(tokenization, axis=1)
-
-additional_tags = [
+tags = [
     "CI",
-    "oai_first_name",
+    "oai_set_specs",
     "resumes.fr",
-    "sujets_rameau_fr",
+    "topics.fr",
     "resumes.en",
-    "sujets_rameau_en",
+    "topics.en",
     "year",
 ]
 
-for col in additional_tags:
-    df[col] = df[col].astype(str)
+for col in tags: df[col] = df[col].astype(str)
 
-ds = Dataset.from_pandas(df.loc[:, additional_tags + ["input_ids", "attention_mask"]])
+ds = Dataset.from_pandas(df.loc[:,tags])
 
-# ====================================================================================
+# ==============================================================================
 
 ds = ds.select(range(10))
 
-# ====================================================================================
+# ==============================================================================
+device = "cuda" if cuda_available() else "cpu"
+print(f"Device : {device}")
+sbert_model = SentenceTransformer(
+    model_name, 
+    device = device, 
+    trust_remote_code = True
+)
+sbert_model.max_seq_length = \
+    AutoConfig.from_pretrained(model_name).max_position_embeddings
 
-embeddings_exporter = ExportEmbeddings(ds, model_name)
-print("device : ", embeddings_exporter.device)
-embeddings_exporter.routine(2, additional_tags, "./theses/embeddings-fr-camembertav2")
+try : 
+    embeddings = (
+        sbert_model
+        .encode(
+            ds["resumes.fr"], 
+            device=str(device), 
+            normalize_embeddings=True, 
+            show_progress_bar=True
+        )
+    )
+    print(embeddings.shape)
+    ds = ds.add_column(
+        "embedding", 
+        [
+            embeddings[i,:].reshape(-1,) 
+            for i in range(embeddings.shape[0])
+        ]
+    )
+    ds.save_to_disk("temp-test")
+except Exception as e:
+    print(e)
+finally:
+    del sbert_model, ds
+    clean()
