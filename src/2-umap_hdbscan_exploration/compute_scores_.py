00c4fd59905f9ca8d0af2e0507a8398f0483577f
A.Morin <axelmorinmail@gmail.com>
2025-10-15 14:18:10 +0200

    indent in a __name__ __main__

--- src/2-umap_hdbscan_exploration/compute_scores_.py
+++ src/2-umap_hdbscan_exploration/compute_scores_.py
@@ -1,25 +1,32 @@
 import pandas as pd
-from dbcv import dbcv
 import numpy as np
+from tqdm import tqdm
+from dbcv import dbcv
 
-record_df = pd.read_csv("./results-umap-hdbscan/record.csv")
+from disco import disco_score
 
-dbcv_scores = []
-n_clusters = []
-taux_noise = []
-for i in range(len(record_df)):
-    index_to_load = record_df.index[i]
-    df = pd.read_csv(f"../../results-umap-hdbscan/results-{index_to_load}.csv")
-    X = np.array([df["x"].to_list(), df["y"].to_list()]).T
-    y = df["hdbscan_clusters"].to_numpy()
-    dbcv_scores += [dbcv(X, y)]
-    n_clusters += [
-        len([c_index for c_index in df["hdbscan_clusters"].unique() if c_index != -1])
-    ]
-    taux_noise += [(df["hdbscan_clusters"] == -1).mean()]
+if __name__ == '__main__':
+    record_df = pd.read_csv("./results-umap-hdbscan/record.csv")
 
-record_df.loc[:, "dbcv_scores"] = dbcv_scores
-record_df.loc[:, "n_clusters"] = n_clusters
-record_df.loc[:, "taux_noise"] = taux_noise
+    dbcv_scores = []
+    disco_scores = []
+    n_clusters = []
+    taux_noise = []
+    for i in tqdm(range(len(record_df))):
+        index_to_load = record_df.index[i]
+        df = pd.read_csv(f"./results-umap-hdbscan/results-{index_to_load}.csv")
+        X = np.array([df["x"].to_list(), df["y"].to_list()]).T
+        y = df["hdbscan_clusters"].to_numpy()
+        dbcv_scores += [dbcv(X, y)]
+        disco_scores += [disco_score(X,y)]
+        n_clusters += [
+            len([c_index for c_index in df["hdbscan_clusters"].unique() if c_index != -1])
+        ]
+        taux_noise += [(df["hdbscan_clusters"] == -1).mean()]
 
-record_df.to_csv("./results-umap-hdbscan/record-with-scores.csv", index = False)
+    record_df.loc[:, "disco_scores"] = disco_scores
+    record_df.loc[:, "dbcv_scores"] = dbcv_scores
+    record_df.loc[:, "n_clusters"] = n_clusters
+    record_df.loc[:, "taux_noise"] = taux_noise
+
+    record_df.to_csv("./results-umap-hdbscan/temp-record-with-scores.csv", index = False)
